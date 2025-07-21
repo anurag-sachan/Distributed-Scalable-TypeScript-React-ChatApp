c900c484e07afae891ca6a1628ed24ed8d5ccb5c
Anurag Sachan <connect2anurags@gmail.com>
2025-07-21 14:31:49 +0530

    ws: broadcasting through 1 server to all the clients

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port:8080});
 let userCount=1;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection",(ws)=>{
+    allSockets.push(ws); //for broadcasting
 
-    console.log("User: "+userCount++);
+    console.log("User "+userCount++ +": connected!");
     ws.on("error", console.error);
 
     ws.on("message",(data)=>{
-        console.log("message received: "+data);
+        console.log(data.toString());
+        allSockets.forEach(socket=>socket.send(data.toString()));
     });
-
-    ws.send("message from server");
 });
